3c92324df8246020228e28f5b02c839621442b75
Kirwa16 <kirwaofficial@gmail.com>
2025-07-14 17:14:41 +0300

    first commit

--- src/data_structures/bst.py
+++ src/data_structures/bst.py
@@ -1,8 +1,9 @@
-class BSTNode:
+class PatientNode:
     def __init__(self, patient):
-        self.patient = patient  # A dictionary e.g. {'id': 5, 'name': 'John'}
+        self.patient = patient  # patient should be a dict with at least an 'id' field
         self.left = None
         self.right = None
+
 
 class PatientBST:
     def __init__(self):
@@ -10,23 +11,63 @@ class PatientBST:
 
     def insert(self, patient):
         def _insert(node, patient):
-            if not node:
-                return BSTNode(patient)
-            if patient['id'] < node.patient['id']:
+            if node is None:
+                return PatientNode(patient)
+            if patient["id"] < node.patient["id"]:
                 node.left = _insert(node.left, patient)
             else:
                 node.right = _insert(node.right, patient)
             return node
         self.root = _insert(self.root, patient)
 
-    def search(self, pid):
-        def _search(node, pid):
-            if not node:
+    def delete(self, patient_id):
+        def _min_value_node(node):
+            current = node
+            while current.left:
+                current = current.left
+            return current
+
+        def _delete(node, patient_id):
+            if node is None:
                 return None
-            if pid == node.patient['id']:
-                return node.patient
-            elif pid < node.patient['id']:
-                return _search(node.left, pid)
+
+            if patient_id < node.patient["id"]:
+                node.left = _delete(node.left, patient_id)
+            elif patient_id > node.patient["id"]:
+                node.right = _delete(node.right, patient_id)
             else:
-                return _search(node.right, pid)
-        return _search(self.root, pid)
+                # Node with one or no children
+                if node.left is None:
+                    return node.right
+                elif node.right is None:
+                    return node.left
+
+                # Node with two children
+                min_node = _min_value_node(node.right)
+                node.patient = min_node.patient
+                node.right = _delete(node.right, min_node.patient["id"])
+
+            return node
+
+        self.root = _delete(self.root, patient_id)
+
+    def search(self, patient_id):
+        def _search(node, patient_id):
+            if node is None:
+                return None
+            if patient_id == node.patient["id"]:
+                return node.patient
+            elif patient_id < node.patient["id"]:
+                return _search(node.left, patient_id)
+            else:
+                return _search(node.right, patient_id)
+
+        return _search(self.root, patient_id)
+
+    def in_order(self):
+        def _in_order(node):
+            if node is None:
+                return []
+            return _in_order(node.left) + [node.patient] + _in_order(node.right)
+
+        return _in_order(self.root)
